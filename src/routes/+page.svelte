b47008ddfa1373d01074e1b4b5740e5b3195a4c4
Calvin Bonner <me@calvinbonner.com>
2024-09-12 10:55:23 -0400

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,31 @@
+<script>
+	import { page } from '$app/stores';
+	let groupExpanded = false;
+</script>
+
+<nav aria-label="Example">
+	<ul role="tree" aria-label="Example">
+		<li role="none">
+			<button
+				role="treeitem"
+				aria-expanded={groupExpanded}
+				aria-owns="example-group"
+				on:click={() => {
+					groupExpanded = !groupExpanded;
+				}}
+			>
+				Example 1
+			</button>
+
+			{#if groupExpanded}
+				<ul role="group">
+					<li role="none">
+						<a href="item-1" role="treeitem" aria-selected={$page.route.id === 'item-1'}>
+							Item 1
+						</a>
+					</li>
+				</ul>
+			{/if}
+		</li>
+	</ul>
+</nav>
